a9a02564300b16b34ad050129f58ff3c3b897ca3
Hengji Chen <jimmy@Hengjis-MacBook-Pro.local>
2020-09-05 16:50:12 -0400

    add the test file for unit testing (test driven development)

---
+++ test_unit_testing.py
@@ -0,0 +1,11 @@
+import pytest
+
+@pytest.mark.parametrize("param1, param2,param3,expected", [
+    ([0,0],[1,1],5,5),
+    ([0,1],[1,0],2,-1),
+    ])
+
+def test_linearest(param1, param2,param3,expected):
+    from func_linearest import linearest
+    answer = linearest(param1, param2, param3)
+    assert answer == expected
